06489ceaebc8b35dd25807adf78bb535a0543881
Enrique Villa <enrique.villa97@hotmail.com>
2020-06-03 02:09:30 -0500

    Final commit(?)

---
+++ numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.20.0'
+version = '1.20.0'
+full_version = '1.20.0.dev0+Unknown'
+git_revision = 'Unknown'
+release = False
+
+if not release:
+    version = full_version
